2004c4e8a0ade30ec91355df54cace0a9bde803b
hamid-manouchehri <hamid.manouchehri.njf@gmail.com>
2026-02-19 06:38:00 -0500

    modify instability_index

--- matlab/AAC/instability_index.m
+++ matlab/AAC/instability_index.m
@@ -1,97 +1,97 @@
-clc;        clear;         close all;
+clc; clear; close all;
 
 dataDirName = '/home/hamid-tuf/projects/powerball/data/admittance_control/test_4_021126/';
-fName_damp10 = [dataDirName 'hm_test_damp_10_damp_10.000000_01.csv'];  % TODO
-fName_damp100 = [dataDirName 'hm_test_damp_100_damp_100.000000_01.csv'];  % TODO
+fName_damp10 = [dataDirName 'hm_test_damp_10_damp_10.000000_01.csv'];
+fName_damp100 = [dataDirName 'hm_test_damp_100_damp_100.000000_01.csv'];
 
-dataset   = fcn_load_csv(fName_damp100);
-t         = dataset.timesteps; 
-Q         = dataset.q; 
-dQ        = dataset.qd; 
-FT        = dataset.FT;
-admit_vel = dataset.vel;  % admittance control velocity; cartesian space
+dataset = fcn_load_csv(fName_damp10);
+t = dataset.timesteps;
+Q = dataset.q;
+dQ = dataset.qd;
+FT = dataset.FT;
 
-%%% change to cartesian-space
-numDataSamples = size(Q, 1);
-ee_pos         = zeros(numDataSamples,3);
-ee_rot         = zeros(numDataSamples,3);
-ee_vel         = zeros(numDataSamples,6);
-ee_accel       = zeros(numDataSamples,6);
-admit_accel    = zeros(numDataSamples,6);
-FTdot          = zeros(numDataSamples,3);
-theta_F        = zeros(numDataSamples,1);
+numDataSamples = size(Q,1);
 
-for i = 1:numDataSamples
-    ee_vel(i, :) = dQ(i, :)*transpose(fcn_Jacob_schunk(Q(i, :)));
-    T = fcn_FK_schunk(Q(i, :));
-    T = T';
-    ee_rot(i, :) = rotm2eul(T(1:3,:), 'ZYX');  % [yaw pitch roll] in radians
-    ee_pos(i, :) = T(4, :);  % measured end-effector velocity
+dt_vec = diff(t);
+dt_med = median(dt_vec);
+Fs = 1/dt_med;  % sampling freq
+L  = numDataSamples;
+
+F_x = FT(:,1);
+
+% HSO parameters:
+Tw = 0.50;  % window length [s] 
+Nw = round(Tw*Fs);  % num samples per windows
+Hop = max(1, round(0.02*Fs));  % update every 20 ms
+lambda = 0.95;  % forgetting factor
+fmax = max(abs(F_x));  % normalize RMS (better: choose expected max force)
+
+fc = 3.0;
+omega0 = 2*pi*(Fs/Nw);  % [rad/s]
+omegaC = 2*pi*fc;
+
+w = hann(Nw);  % window to reduce leakage (recommended)
+
+% time indices where we can compute a full window
+k0 = Nw;
+k_list = k0:Hop:L;
+
+Iv = nan(size(k_list));
+Is = nan(size(k_list));
+Ifrms = nan(size(k_list));
+
+Is_prev = 0;
+
+for ii = 1:numel(k_list)
+    k = k_list(ii);
+
+    xw = F_x(k-Nw+1:k);
+    xw = xw - mean(xw); % remove DC (important)
+    xw = xw .* w;
+
+    X = fft(xw);
+    P = abs(X)/Nw;  % amplitude spectrum (consistent with your P2=abs(X)/L)
+    P1 = P(1:floor(Nw/2)+1);  % one-sided
+
+    f_bins = (0:floor(Nw/2))*(Fs/Nw);
+
+    % exclude DC bin in denominator (start at bin 2 in MATLAB indexing)
+    idx_total = 2:numel(P1);  % f > 0
+    idx_hf    = find(f_bins >= fc, 1, 'first'):numel(P1);
+
+    denom = sum(P1(idx_total)) + eps;
+    numer = sum(P1(idx_hf)) + eps;
+
+    Iv(ii) = numer/denom; % Eq(4) HSO
+
+    rmsf = sqrt(mean((xw).^2));
+    Ifrms(ii) = min(max(rmsf/(fmax+eps), 0), 1);  % Eq(6), clipped [0,1]
+
+    Is_curr = Iv(ii)*Ifrms(ii) + lambda*Is_prev;  % Eq(5)
+    Is(ii) = Is_curr;
+    Is_prev = Is_curr;
 end
 
-dt = diff(t);
-M_admit = 0.3*diag([1, 1, 1]);
-C_admit = 10*diag([1.2, 1.0, 1.0]);
+t_idx = t(k_list);
 
+% plots
 figure;
+
 subplot(3,1,1);
-plot(t, FT(:,1));
-xlabel("time (s)"); ylabel("F_x (N)");
-
-Fs = floor(numDataSamples / t(end));  % sampling freq
-T = 1 / Fs;  % sampling period
-L = numDataSamples;  % Length of signal
-
-x = FT(:,1) - mean(FT(:,1));
-X = fft(x);
-f = (0:L-1)*(Fs/L);
-
-% one-sided amplitude spectrum
-P2 = abs(X)/L;
-P1 = P2(1:floor(L/2)+1);
-P1(2:end-1) = 2*P1(2:end-1);
-f1 = f(1:floor(L/2)+1);
-
-mag_dB = 20*log10(P1 + eps);  % eps avoids -Inf
+plot(t, F_x, 'LineWidth', 1);
+grid on; xlabel('time (s)'); ylabel('Force (N)');
+title('Force signal used for instability index');
 
 subplot(3,1,2);
-semilogx(f1, mag_dB); grid on;
-xlabel('Frequency (Hz)'); ylabel('Magnitude (dB)');
+plot(t_idx, Iv, 'LineWidth', 1);
+grid on; xlabel('time (s)'); ylabel('I_v');
+title(sprintf('HSO index I_v (fc = %.1f Hz)', fc));
+ylim([0 1]);
 
-f0 = 0.01;
-fc = 3;
-omega0 = 2*pi*f0;
-omegaC = 2*pi*fc;  % cut-off frequency (rad/s)
+subplot(3,1,3);
+plot(t_idx, Is, 'LineWidth', 1);
+grid on; xlabel('time (s)'); ylabel('I_s');
+title(sprintf('Improved instability index I_s (\\lambda=%.2f)', lambda));
 
-window_time = 0.1;  % seconds TODO
-num_sample_per_windows = round(window_time*numDataSamples/max(t));
-num_windows = round(max(t) / window_time);
-Is = zeros(num_windows,1);
-
-j = 1;
-N = size(FT,1);
-
-for i = 1:num_windows
-    idxEnd = j + num_sample_per_windows - 1;
-
-    if idxEnd > N
-        break;   % or error('Not enough data for last window')
-    end
-
-    x = FT(j:idxEnd, 1);
-    X = fft(x);
-
-    P2 = abs(X/num_sample_per_windows);
-    P1 = P2(1:floor(num_sample_per_windows/2)+1);
-    P1(2:end-1) = 2*P1(2:end-1);
-
-    id = P1 > fc;
-    Is(i) = sum(P1(id)) / sum(P1);
-
-    f = Fs/num_sample_per_windows*(0:floor(num_sample_per_windows/2));
-    j = j + num_sample_per_windows;
-end
-
-subplot(3,1,3)
-t_of_index = window_time:window_time:max(t);
-plot(t_of_index, Is)
+fprintf('Fs = %.3f Hz, Nw = %d samples, Tw = %.3f s\n', Fs, Nw, Nw/Fs);
+fprintf('omega0 = %.3f rad/s, omegaC = %.3f rad/s\n', omega0, omegaC);
