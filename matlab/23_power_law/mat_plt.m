38ddd02edf26e00b1b7e6ed73cb46e2951db533a
hamid-manouchehri <hamid.manouchehri.njf@gmail.com>
2026-03-09 18:51:15 -0400

    calculate global entropy

--- matlab/23_power_law/mat_plt.m
+++ matlab/23_power_law/mat_plt.m
@@ -10,41 +10,41 @@ hand_written_drawing = load(hand_written_file);
 schunk_admittance = load(schunk_file);
 
 % hand-written (hw) data:
-hw_t = hand_written_drawing.t;
-hw_x = hand_written_drawing.x;
-hw_y = hand_written_drawing.y;
-hw_R = hand_written_drawing.R;
-hw_K = hand_written_drawing.K_hat;
-hw_beta = hand_written_drawing.beta_hat;
-hw_window_center_time = hand_written_drawing.window_center_time;
+t_hw = hand_written_drawing.t;
+x_hw = hand_written_drawing.x;
+y_hw = hand_written_drawing.y;
+R_hw = hand_written_drawing.R;
+K_hw = hand_written_drawing.K_hat;
+beta_hw = hand_written_drawing.beta_hat;
+window_center_time_hw = hand_written_drawing.window_center_time;
 
 % Schunk admittance control data:
-schunk_t = schunk_admittance.schunk_time_s;
-schunk_x = schunk_admittance.x;
-schunk_y = schunk_admittance.y;
-schunk_R = schunk_admittance.R;
-schunk_K = schunk_admittance.K_hat;
-schunk_beta = schunk_admittance.beta_hat;
-schunk_window_center_time = schunk_admittance.window_center_time;
+t_schunk = schunk_admittance.schunk_time_s;
+x_schunk = schunk_admittance.x;
+y_schunk = schunk_admittance.y;
+R_schunk = schunk_admittance.R;
+K_schunk = schunk_admittance.K_hat;
+beta_schunk = schunk_admittance.beta_hat;
+window_center_time_schunk = schunk_admittance.window_center_time;
 
 figure;
 subplot(1,2,1);
-plot(hw_x, hw_y, "r");
+plot(x_hw, y_hw, "r");
 % xlabel("x (m)");
 % ylabel("y (m)");
 title("hand-written path.")
 subplot(1,2,2);
-plot(schunk_y, schunk_x, "r")
+plot(y_schunk, x_schunk, "r")
 title("admittance controlled path.")
 % figure;
-% plot(hw_window_center_time, hw_beta, 'r.-');
+% plot(window_center_time_hw, beta_hw, 'r.-');
 % grid on;
 % xlabel('time [s]');
 % ylabel('\beta');
 % title('Segment-wise power law exponent');
 
 % figure;
-% plot(hw_window_center_time, hw_K, 'r.-');
+% plot(window_center_time_hw, K_hw, 'r.-');
 % grid on;
 % xlabel('time [s]');
 % ylabel('K');
@@ -53,34 +53,45 @@ title("admittance controlled path.")
 figure;
 subplot(1,2,1)
 % beta_error = abs(1/3 - hw_beta);
-histogram(hw_beta,-1:0.01:1,'Normalization','probability');
+histogram(beta_hw,-1:0.01:1,'Normalization','probability');
 grid on;
 % xlabel('time [s]');
 ylabel('\beta hat');
 title('hand written Beta');
 subplot(1,2,2);
-histogram(schunk_beta,-1:0.01:1,'Normalization','probability');
+histogram(beta_schunk,-1:0.01:1,'Normalization','probability');
 title('schunk admittance Beta; C=50');
 grid on;
 
 figure;
 subplot(1,2,1);
-plot(hw_R, "o");
+plot(R_hw, "o");
 title("R hand-written");
 ylim([0,6]);
 subplot(1,2,2);
-plot(schunk_R, "o");
+plot(R_schunk, "o");
 title("R admittance control");
 
 figure;
-subplot(1,2,1)
-plot(hw_K,'o');
-ylim([0 10])
+subplot(1,2,1);
+plot(K_hw,'o');
+ylim([0 10]);
+title("K hand written");
+subplot(1,2,2);
+plot(K_schunk,'o');
+title("K shcunk");
 
 figure;
 subplot(1,2,1)
-p_hw = histogram(hw_K/10, 0:0.001:.2, "Normalization","probability");
-entropy_hw = -sum(p_hw.Values.*log2(p_hw.Values));
+K_hw_valid = K_hw(~isnan(K_hw) & ~isinf(K_hw));
+histogram(K_hw_valid/10, 0:0.001:.2, "Normalization","probability");
+[pmf_hw, edges_hw] = histcounts(K_hw_valid/10, "Normalization","probability");
+pmf_hw = pmf_hw(pmf_hw > 0);
+entropy_hw = -sum(pmf_hw.*log2(pmf_hw));
+
 subplot(1,2,2)
-p_schunk = histogram(schunk_K, 0:0.001:.2, "Normalization","probability");
-entropy_schunk = -sum(p_schunk.Values.*log2(p_schunk.Values));
+K_schunk_valid = K_schunk(~isnan(K_schunk) & ~isinf(K_schunk));
+histogram(K_schunk_valid, 0:0.001:.2, "Normalization","probability");
+[pmf_schunk, edges_schunk] = histcounts(K_schunk_valid, "Normalization","probability");
+pmf_schunk = pmf_schunk(pmf_schunk > 0);
+entropy_schunk = -sum(pmf_schunk.*log2(pmf_schunk));
